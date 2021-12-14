c19b5c161c6878178a6d5ef30ac32014d36f2907
wildone <max.barrass@gmail.com>
2021-12-14 23:34:43 +1100

    refactor to latest docker-compose.

--- start.ps1
+++ start.ps1
@@ -1,6 +1,7 @@
 Param(
   [string]$FUNCTIONS_URI = "https://github.com/aem-design/aemdesign-docker/releases/latest/download/functions.ps1",
-  [string]$SERVICES = "$( (Get-Content ".\start-services.conf") -join " -f ")"
+  [string]$SERVICES = "$( (Get-Content ".\start-services.conf") -join " -f ")",
+  [string]$PORT = "5080"
 )
 
 $SKIP_CONFIG = $true
@@ -8,12 +9,23 @@ $PARENT_PROJECT_PATH = ".."
 
 . ([Scriptblock]::Create((([System.Text.Encoding]::ASCII).getString((Invoke-WebRequest -Uri "${FUNCTIONS_URI}").Content))))
 
+
+# get env config
+$CONFIG_ENV = (Get-Content ".env" -Raw | ConvertFrom-StringData)
+$PORT=$CONFIG_ENV.TRAEFIK_HTTP_PORT
+
 $DOCKER_COMMAND="docker-compose --env-file .env -f docker-compose.yml -f ${SERVICES} up"
 
-printSectionLine "${DOCKER_COMMAND}"
+$OPEN_COMMAND="explorer http://localhost:${PORT}"
+
+printSubSectionStart "Open Console at http://localhost:${PORT}"
+
+Invoke-Expression "${OPEN_COMMAND}"
 
 
 printSubSectionStart "Docker Compose Execute"
+
+printSectionLine "${DOCKER_COMMAND}"
 
 Invoke-Expression "${DOCKER_COMMAND}"
 
