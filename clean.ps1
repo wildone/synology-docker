cbaa937b8526abc6fb673f971292278884529f79
wildone <max.barrass@gmail.com>
2021-12-14 23:51:38 +1100

    add clean script.

---
+++ clean.ps1
@@ -0,0 +1,17 @@
+Param(
+  [string]$FUNCTIONS_URI = "https://github.com/aem-design/aemdesign-docker/releases/latest/download/functions.ps1",
+  [array]$CLEAN_PATHS = (Get-Content ".\clean-paths.conf")
+)
+
+$SKIP_CONFIG = $true
+$PARENT_PROJECT_PATH = "."
+
+. ([Scriptblock]::Create((([System.Text.Encoding]::ASCII).getString((Invoke-WebRequest -Uri "${FUNCTIONS_URI}").Content))))
+
+printSubSectionStart "Remove data paths"
+
+Foreach ($REMOVE_PATH in $CLEAN_PATHS) {
+  printSectionLine "Removing ${REMOVE_PATH}"
+  Remove-Item –path $REMOVE_PATH –recurse -ErrorAction SilentlyContinue
+}
+
