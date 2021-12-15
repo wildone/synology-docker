7de6317119754ff395a22208683d04ef85d4f5b3
wildone <max.barrass@gmail.com>
2021-12-16 10:58:00 +1100

    refactor to add logging.

--- services/data/postgres/config/pgpassconfig.sh
+++ services/data/postgres/config/pgpassconfig.sh
@@ -1,6 +1,34 @@
-set -o allexport; source /pgadmin4/env; set +o allexport;
-mkdir -m 700 /var/lib/pgadmin/storage/pgadmin_localhost.com;
-chown -R pgadmin:pgadmin /var/lib/pgadmin/storage/pgadmin_localhost.com;
-#cp -prv /pgadmin4/pgpass /var/lib/pgadmin/storage/pgadmin_localhost.com/;
-echo ${POSTGRES_HOST}:${POSTGRES_PORT}:${POSTGRES_DB}:${POSTGRES_DB_USER}:${POSTGRES_DB_PASSWORD}>/var/lib/pgadmin/storage/pgadmin_localhost.com/pgpass
-chmod 600 /var/lib/pgadmin/storage/pgadmin_localhost.com/pgpass;
+set -o allexport
+source /pgadmin4/env
+set +o allexport;
+
+LOG_FILE="/tmp/pgpassconfig.log"
+USER_FOLDER_NAME="${POSTGRES_DB_ADMIN_PGADMIN_DEFAULT_EMAIL//[@]/_}"
+USER_FOLDER="/var/lib/pgadmin/storage/${USER_FOLDER_NAME}"
+USER_PGPASS_FILE="${USER_FOLDER}/pgpass"
+DATA_FODLER="/var/lib/pgadmin"
+
+echo "Createting pgpass for user ${USER_FOLDER}" >>${LOG_FILE}
+
+ls -l ${DATA_PATH_HOST} >>${LOG_FILE}
+
+mkdir -m 700 -p ${USER_FOLDER}
+
+echo "Folder created $(ls -latr ${USER_FOLDER})" >>${LOG_FILE}
+
+chown -R pgadmin:pgadmin ${USER_FOLDER}
+chown -R pgadmin:pgadmin ${DATA_FODLER}
+
+touch ${USER_PGPASS_FILE}
+
+echo "File created $(cd ${USER_FOLDER} && ls -latr )" >>${LOG_FILE}
+
+echo ${POSTGRES_HOST}:${POSTGRES_PORT}:${POSTGRES_DB}:${POSTGRES_DB_USER}:${POSTGRES_DB_PASSWORD} >${USER_PGPASS_FILE}
+
+echo "File updated $(cd ${USER_FOLDER} && ls -latr )" >>${LOG_FILE}
+
+echo ${POSTGRES_HOST}:${POSTGRES_PORT}:${POSTGRES_DB}:${POSTGRES_DB_USER}:${POSTGRES_DB_PASSWORD} >>${LOG_FILE}
+
+chmod 600 ${USER_PGPASS_FILE}
+
+echo "File updated $(cd ${USER_FOLDER} && ls -latr )" >>${LOG_FILE}
